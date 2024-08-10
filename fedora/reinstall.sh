fde322aecb19502ffab06b9392b1a1788a11e60c
Lucas de Lima <lucasdelima@gmail.com>
2024-08-09 22:18:21 -0300

    Update reinstall.sh

--- fedora/reinstall.sh
+++ fedora/reinstall.sh
@@ -26,6 +26,8 @@ fedora_apps=(
     "Lutris"            # game manager
     "steam"             # steam
     "github-desktop"    # github client
+    "syncthing"         # sync app
+    "picard"            # music tagger
 )
 for n in ${fedora_apps[@]}
 do
@@ -46,22 +48,23 @@ sudo -v && wget -nv -O- https://download.calibre-ebook.com/linux-installer.sh | 
 # Flatpak app
 #############################
 flatpak_apps=(
-    "de.haeckerfelix.Fragments"                       # torrent client
-    "com.felipekinoshita.Kana"                        # japanese hiragana/katakana
-    "info.febvre.Komikku"                             # comic reader
-    "org.gnome.Solanum"                               # pomodoro clock
-    "com.mattjakeman.ExtensionManager"                # gnome extensions manager
-    "io.bassi.Amberol"                                # music player
-    "io.github.flattool.Warehouse"                    # flatpak manager
-    "io.github.giantpinkrobots.flatsweep"             # flatpak cleaner
-    "com.github.tchx84.Flatseal"                      # flatpak permissions manager
-    "it.mijorus.gearlever"                            # appimages manager
-    "ca.desrt.dconf-editor"                           # dconf editor
-    "io.gitlab.adhami3310.Converter"                  # image converter
-    "org.gnome.gitlab.YaLTeR.VideoTrimmer"            # video trimmer
-    "net.davidotek.pupgui2"                           # proton manager
-    "flathub com.vscodium.codium"                     # code editor
-    "page.codeberg.libre_menu_editor.LibreMenuEditor" # menu editor
+    "de.haeckerfelix.Fragments"                          # torrent client
+    "com.felipekinoshita.Kana"                           # japanese hiragana/katakana
+    "info.febvre.Komikku"                                # comic reader
+    "org.gnome.Solanum"                                  # pomodoro clock
+    "com.mattjakeman.ExtensionManager"                   # gnome extensions manager
+    "io.bassi.Amberol"                                   # gnome music player
+    "flatpak install flathub com.github.taiko2k.tauonmb" # music player
+    "io.github.flattool.Warehouse"                       # flatpak manager
+    "io.github.giantpinkrobots.flatsweep"                # flatpak cleaner
+    "com.github.tchx84.Flatseal"                         # flatpak permissions manager
+    "it.mijorus.gearlever"                               # appimages manager
+    "ca.desrt.dconf-editor"                              # dconf editor
+    "io.gitlab.adhami3310.Converter"                     # image converter
+    "org.gnome.gitlab.YaLTeR.VideoTrimmer"               # video trimmer
+    "net.davidotek.pupgui2"                              # proton manager
+    "flathub com.vscodium.codium"                        # code editor
+    "page.codeberg.libre_menu_editor.LibreMenuEditor"    # menu editor
     )
 for n in ${flatpak_apps[@]};
 do
@@ -93,6 +96,23 @@ gnome_ext=(
     "https://extensions.gnome.org/extension/7065/tiling-shell/"
 )
 for n in ${gnome_ext[@]}
+do
+    xdg-open "$n"
+done
+
+#############################
+# Other Downloads
+#############################
+other_downloads=(
+    "https://github.com/Automattic/simplenote-electron/releases/latest/" # Simplenote
+    "https://www.blender.org/download/"                                  # Blender
+    "https://krita.org/en/download/"                                     # Krita
+    "https://godotengine.org/download/linux/"                            # Godot
+    "https://www.pureref.com/download.php"                               # Pureref
+    "https://app.gumroad.com/d/904c6b0ca8a2d4c38c046871e4546690"         # Aseprite
+    "https://www.slsknet.org/news/"                                      # Soulseek
+)
+for n in ${other_downloads[@]}
 do
     xdg-open "$n"
 done
